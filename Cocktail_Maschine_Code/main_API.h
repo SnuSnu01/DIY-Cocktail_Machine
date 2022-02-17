315af87e3c7247ec21ad28157d1c32f058e7d784
SnuSnu01 <99464102+SnuSnu01@users.noreply.github.com>
2022-02-17 04:48:41 +0100

    Add files via upload

--- Cocktail_Maschine_Code/main_API.h
+++ Cocktail_Maschine_Code/main_API.h
@@ -2,3 +2,63 @@
 // Author :
 // Date   : 17.02.2022
 // Version: 0.0.1
+
+void LongIsland (){
+  
+}
+
+void SexOnTheBeach (){
+  
+}
+
+void CubaLibre (){
+  
+}
+
+void CranberryVodka (){
+  
+}
+
+void Screwdriver (){
+  
+}
+
+void BlueLady (){
+  
+}
+
+void BlackThunder (){
+  
+}
+
+void KickItIn (){
+  
+}
+
+void (AquaVelva){
+  
+}
+
+void (SapphireMadras){
+  
+}
+
+void (GreenWidow){
+  
+}
+
+void (GreenEyes){
+  
+}
+
+void (GreenDevil){
+  
+}
+
+void (GinJuice){
+  
+}
+
+void (SoftPoison){
+  
+}
