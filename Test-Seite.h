530201a43a1a1f552bcaf79405c6c56359b23225
X-Battle-Pilot <100043431+X-Battle-Pilot@users.noreply.github.com>
2022-02-21 00:32:11 +0100

    Add files via upload

---
+++ Test-Seite.h
@@ -0,0 +1,31 @@
+const char LongIslandIceTea[] = R"=====(
+<!DOCTYPE HTML>
+
+<html lang="de">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="author" content="Kevin Kilian">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="platzhalter.css">
+    <style>
+        p {
+            color: black;
+        }
+    </style>
+
+    <script></script>
+    <noscript></noscript>
+
+    <base href="khe-Cocktails" target="_self">
+
+  </head><body>
+  <h1>khe-Cocktails</h1>
+  <h2>Getränkeliste</h2>
+  <h3>Long Island Ice Tea</h3>
+  <button><img src="Long-Island-Iced-Tea-icon.png" height ="200" width="200" /><p>Long Island Ice Tea</p></button>
+
+
+</body>
+</html>)=====";
